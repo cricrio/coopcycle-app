56e9587dcbce000d81f6fb79d7d413633d9e0c4b
Alexandre Segura <mex.zktk@gmail.com>
2020-09-26 19:05:06 +0200

    WIP

--- src/navigation/restaurant/Order.js
+++ src/navigation/restaurant/Order.js
@@ -313,7 +313,7 @@ const styles = StyleSheet.create({
 function mapStateToProps(state, ownProps) {
 
   return {
-    order: ownProps.navigation.getParam('order'),
+    order: ownProps.route.params?.order,
     isPrinterConnected: !!state.restaurant.printer,
     printer: state.restaurant.printer,
   }
